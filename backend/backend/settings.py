7a76c61f39afd7ef0bf783776325037c9012f23f
Felipe Americo <felipe.americo@gmail.com>
2022-11-27 10:31:04 -0300

    Deploy Heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#_u@wxojecajtibc0w@=gi-0@)a411n5d70u0l1nt1@y^mlmgn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
 # Use this when production to Heroku
-# DEBUG = False
-# ALLOWED_HOSTS = ['.herokuapp.com']
+DEBUG = False
+ALLOWED_HOSTS = ['localhost', 'bololandia.herokuapp.com']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,8 @@ USE_TZ = True
 CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
