310e1a3a12929eaabca2bc6de270b9a2568673d6
Felipe Americo <felipe.americo@gmail.com>
2022-12-04 13:37:21 -0300

    Fixed Debug False

--- backend/settings.py
+++ backend/settings.py
@@ -24,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#_u@wxojecajtibc0w@=gi-0@)a411n5d70u0l1nt1@y^mlmgn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 # ALLOWED_HOSTS = []
 
 # Use this when production to Render
-# DEBUG = False
-ALLOWED_HOSTS = ['localhost', 'bololandia.onrender.com']
-
+DEBUG = False
+ALLOWED_HOSTS = ['bololandia.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
@@ -146,4 +145,4 @@ GRAPH_MODELS = {
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
