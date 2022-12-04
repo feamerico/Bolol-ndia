f7a041ff63b158a9a413287831509f4bfbc1f5a6
Felipe Americo <felipe.americo@gmail.com>
2022-12-04 02:11:52 -0300

    Fixing Req.txt

--- backend/settings.py
+++ backend/settings.py
@@ -25,11 +25,11 @@ SECRET_KEY = 'django-insecure-#_u@wxojecajtibc0w@=gi-0@)a411n5d70u0l1nt1@y^mlmgn
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 # Use this when production to Heroku
 # DEBUG = False
-# ALLOWED_HOSTS = ['localhost', 'bololandia.onrender.com']
+ALLOWED_HOSTS = ['localhost', 'bololandia.onrender.com']
 
 
 # Application definition
